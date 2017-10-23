0b9dc3103c1cb9472b2898f986c35f99a514030d
Felix Astner <astner.felix@live.de>
2017-10-23 20:17:15 +0200

    converts white and black colors into heights

--- src/main/java/algorithm/ColorPointConversion.java
+++ src/main/java/algorithm/ColorPointConversion.java
@@ -1,8 +1,16 @@
 package algorithm;
+
 import javafx.geometry.Point3D;
 
 public class ColorPointConversion {
-    static public Point3D convertColorToPoint() {
-        return new Point3D(0,0,0);
+
+    public static final int MAX_VALUE = 100;
+    public static final int MIN_VALUE = 0;
+
+    static public Point3D convertColorToPoint(int i) {
+        if (i >= 255) {
+            return new Point3D(0, 0, MAX_VALUE);
+        }
+        return new Point3D(0, 0, MIN_VALUE);
     }
 }
--- src/test/java/algorithm/ColorPointConversionTest.java
+++ src/test/java/algorithm/ColorPointConversionTest.java
@@ -1,13 +1,25 @@
 package algorithm;
 
+import javafx.geometry.Point3D;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
+import static algorithm.ColorPointConversion.MAX_VALUE;
+import static algorithm.ColorPointConversion.MIN_VALUE;
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
 class ColorPointConversionTest {
 
+
     @Test
-    void testsBlackPixelReturningZero() {
-        assertNotNull(ColorPointConversion.convertColorToPoint());
+    void testsBlackAndWhitePixel() {
+        final Point3D whitePoint = ColorPointConversion.convertColorToPoint(255);
+        final Point3D blackPoint = ColorPointConversion.convertColorToPoint(0);
+
+        assertEquals(new Point3D(0, 0, MAX_VALUE), whitePoint);
+        assertEquals(new Point3D(0, 0, MIN_VALUE), blackPoint);
+
     }
 }
